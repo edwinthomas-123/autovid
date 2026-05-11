082f65009962df8ebee17f9e886659c819c58f11
edwinthomas-123 <edwinmoothedan2006@gmail.com>
2026-05-11 21:16:34 +0530

    Fix: Re-implement Google Login and Edwin bypass

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="464352778695-8icdg1jhc9j9t8un521it9jcpum84bca.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
